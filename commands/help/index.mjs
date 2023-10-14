1b94e666176b2f3b367147d6950752f8a01b206f
NiceDayTooYou <88866636+NiceDayTooYou@users.noreply.github.com>
2023-10-14 12:22:24 +0800

    Update index.mjs

--- commands/help/index.mjs
+++ commands/help/index.mjs
@@ -1,6 +1,6 @@
 const output = [
   "可用命令/Available commands:",
-  "profile/信息, skills/单词, projects/页面, contacts/联系, about/关于, help/帮助, dino/像素恐龙, hackerman/点击键盘, matrix, screensaver/屏幕保护程序, cowsay/颜文字, joke/笑话, fire/火焰, clear/清除, reboot/重启, quit/退出",
+  "profile/信息, skills/单词, projects/页面, contacts/联系, about/关于, help/帮助, dino/像素恐龙, hackerman/点击键盘, matrix/矩阵, screensaver/屏幕保护程序, cowsay/颜文字, joke/笑话, fire/火焰, clear/清除, reboot/重启, quit/退出",
 ];
 
 export { output };
